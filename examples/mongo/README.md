270f62997d187a6fc73d3c9ff8c8ca3447f663f5
chadicus <chadwickgray@gmail.com>
2015-07-27 19:40:46 -0400

    Update example to include scope and POST

--- examples/mongo/README.md
+++ examples/mongo/README.md
@@ -14,8 +14,74 @@ php -S localhost:8888 -t examples/mongo examples/mongo/index.php
 ```sh
 curl -X POST -d client_id=librarian -d client_secret=secret -d grant_type=client_credentials http://localhost:8888/token
 ```
+### Output
+```
+{
+    "access_token": "c859d2c9eec4800a6277671eba72a5a6f54f8693",
+    "expires_in": 3600,
+    "token_type": "Bearer",
+    "scope": "bookCreate"
+}
+```
 
 ## Use the access token to search the books endpoint
 ```sh
-curl -H 'Authorization: Bearer access_token' http://localhost:8888/books
+curl -H 'Authorization: Bearer c859d2c9eec4800a6277671eba72a5a6f54f8693' http://localhost:8888/books
 ```
+
+### Output
+```
+{
+    "offset": 0,
+    "limit": 5,
+    "total": 11,
+    "books": [
+        {
+            "id": "55b6bcdd498b62a2158b4567",
+            "url": "/books/55b6bcdd498b62a2158b4567"
+        },
+        {
+            "id": "55b6bcdd498b62a2158b4568",
+            "url": "/books/55b6bcdd498b62a2158b4568"
+        },
+        {
+            "id": "55b6bcdd498b62a2158b4569",
+            "url": "/books/55b6bcdd498b62a2158b4569"
+        },
+        {
+            "id": "55b6bcdd498b62a2158b456a",
+            "url": "/books/55b6bcdd498b62a2158b456a"
+        }
+    ]
+}
+```
+
+## Create a new book
+```sh
+curl -i -X POST -H Content-Type:application/json -H 'Authorization: Bearer c859d2c9eec4800a6277671eba72a5a6f54f8693' -d @mongo/create.json http://localhost:8888/books
+```
+
+### Output
+
+The body of the response will be empty.  The http status code will be `201` and there will a location header with the url for the new book `Location: /books/55b6be5e498b6293138b456f`
+
+## Get the details of the newly created book
+
+```sh
+curl -H 'Authorization: Bearer c859d2c9eec4800a6277671eba72a5a6f54f8693' http://localhost:8888/books/55b6be5e498b6293138b456f
+```
+
+### Output
+```
+{
+    "author": "Galos, Mike",
+    "title": "Visual Studio 7: A Comprehensive Guide",
+    "genre": "Computer",
+    "price": 49.95,
+    "publishDate": 987393600,
+    "description": "Microsoft Visual Studio 7 is explored in depth, looking at how Visual Basic, Visual C++, C#, and ASP+ are integrated into a comprehensive development environment.",
+    "id": "55b6be5e498b6293138b456f",
+    "url": "/books/55b6be5e498b6293138b456f"
+}
+```
+
--- examples/mongo/index.php
+++ examples/mongo/index.php
@@ -3,8 +3,7 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Chadicus\Slim\OAuth2\Routes;
-use Chadicus\Slim\OAuth2\Middleware;
-use Slim\Slim;
+use Chadicus\Slim\OAuth2\Middleware; use Slim\Slim;
 use OAuth2\Server;
 use OAuth2\Storage;
 use OAuth2\GrantType;
@@ -12,7 +11,8 @@ use OAuth2\GrantType;
 $mongoDb = (new MongoClient())->selectDb('slim_oauth2');
 
 $storage = new Storage\Mongo($mongoDb);
-$storage->setClientDetails('librarian', 'secret');
+$storage->setClientDetails('librarian', 'secret', null, null, 'bookCreate');
+$storage->setClientDetails('student', 's3cr3t');
 
 $server = new Server(
     $storage,
@@ -35,18 +35,24 @@ $authorization->setApplication($app);
 $app->get('/books', $authorization, function () use ($app, $mongoDb) {
     $result = [];
     try {
-        $limit = isset($_GET['limit']) ? $_GET['limit'] : 5;
-        $offset = isset($_GET['offset']) ? $_GET['offset'] : 0;
+        $limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 5;
+        $offset = isset($_GET['offset']) ? (int)$_GET['offset'] : 0;
 
-        $books = $mongoDb->books->find([], ['_id' => false])->skip($offset)->limit($limit);
+        $books = $mongoDb->books->find([])->skip($offset)->limit($limit);
 
         $result = [
             'offset' => $offset,
             'limit' => $books->count(true),
             'total' => $books->count(),
-            'result' => iterator_to_array($books),
+            'books' => [],
         ];
 
+        foreach ($books as $book) {
+            $result['books'][] = [
+                'id' => (string)$book['_id'],
+                'url' => "/books/{$book['_id']}",
+            ];
+        }
     } catch (\Exception $e) {
         $app->response()->status(400);
         $result = ['error' => $e->getMessage()];
@@ -57,14 +63,26 @@ $app->get('/books', $authorization, function () use ($app, $mongoDb) {
 })->name('books-search');
 
 $app->get('/books/:id', $authorization, function ($id) use ($app, $mongoDb) {
-    $book = $mongoDb->books->findOne(['id' => $id], ['_id' => false]);
+    $book = $mongoDb->books->findOne(['_id' => new \MongoId($id)]);
     if ($book === null) {
         $app->response()->status(404);
         $book = ['error' => "Book with id '{$id}' was not found"];
     }
 
+    $book['id'] = (string)$book['_id'];
+    $book['url'] = "/books/{$book['_id']}";
+    unset($book['_id']);
+
     $app->contentType('application/json');
     $app->response->setBody(json_encode($book));
 })->name('books-detail');
+
+$app->post('/books', $authorization->withRequiredScope(['bookCreate']), function () use ($app, $mongoDb) {
+    $book = json_decode($app->request->getBody(), true);
+
+    $mongoDb->books->insert($book);
+    $app->response()->status(201);
+    $app->response()->headers->set('Location', "/books/{$book['_id']}");
+});
 
 $app->run();
